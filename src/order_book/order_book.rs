1411cdcfcc7589ec217a541f8397c9b3a20f29e2
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-08-19 23:48:48 -0400

    adding blank and from snapshot constructors

--- src/order_book/order_book.rs
+++ src/order_book/order_book.rs
@@ -1,3 +1,5 @@
+use std::cell::RefCell;
+
 use super::updates::{Side, Trader};
 
 pub struct Order {
@@ -6,6 +8,27 @@ pub struct Order {
 }
 
 pub struct OrderBook {
-    asks: [Vec<Order>;99],
-    bids: [Vec<Order>;99],
+    asks: RefCell<[Vec<Order>;99]>,
+    bids: RefCell<[Vec<Order>;99]>,
+}
+
+impl OrderBook {
+    fn create_blank_ladders() -> ([Vec<Order>; 99], [Vec<Order>; 99]) {
+        let ask_ladder: [Vec<Order>; 99] = std::array::from_fn(|_| Vec::new());
+        let bid_ladder: [Vec<Order>; 99] = std::array::from_fn(|_| Vec::new());
+        (ask_ladder, bid_ladder)
+    }   
+
+    pub fn new_blank() -> Self{
+        let (ask_ladder, bid_ladder) = OrderBook::create_blank_ladders();
+        return Self { asks: RefCell::new(ask_ladder), bids: RefCell::new(bid_ladder)}
+    }
+    
+    pub fn from_snapshot(asks: Vec<(u8, i32)>, bids: Vec<(u8, i32)>) -> Self {
+        let (mut ask_ladder, mut bid_ladder) = OrderBook::create_blank_ladders();
+        
+        // TODO: fill in ladders
+        return Self { asks: RefCell::new(ask_ladder), bids: RefCell::new(bid_ladder)}
+    }
+
 }
