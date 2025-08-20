3e714317e34e3db2792f81f195837cbdf713669b
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-08-19 23:32:12 -0400

    add peek next time step

--- src/time_keeping/timer.rs
+++ src/time_keeping/timer.rs
@@ -10,7 +10,18 @@ impl Timer {
         Self { time: RefCell::new(start_time), delta: delta  }
     }
 
+    // increment time to the next time
     pub fn increment(&self) {
         *self.time.borrow_mut() += self.delta
+    }
+
+    // peek what the next time step will be
+    pub fn peek_next_time(&self) -> f64 {
+        *self.time.borrow_mut() + self.delta
+    }
+
+    // get the current time step
+    pub fn get_time(&self) -> f64 {
+        *self.time.borrow_mut()
     }
 }
