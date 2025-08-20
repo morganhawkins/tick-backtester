3bf261439de34a0672ca1e5be31c4cb2c79ea7b5
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-08-19 23:36:43 -0400

    adding latency constant to action producer

--- src/actions/action_producer.rs
+++ src/actions/action_producer.rs
@@ -5,21 +5,22 @@ use crate::time_keeping::timer::Timer;
 use super::actions::Action;
 
 pub struct ActionProducer {
-    timer: Rc<Timer>,
+    timer: Rc<Timer>, // used to track simulation time
+    latency_constant: f64, // represents the message send latency
 }
 
 impl ActionProducer {
-    pub fn new(timer: &Rc<Timer>) -> Self {
-        Self{timer: Rc::clone(timer)}
+    pub fn new(timer: &Rc<Timer>, latency_constant: f64) -> Self {
+        Self{timer: Rc::clone(timer), latency_constant: latency_constant}
     }
 
     pub fn order_place(&self, price: u8, quantity: i32, side: Side) -> Action {
-        let ts= self.timer.get_time();
+        let ts= self.timer.get_time() + self.latency_constant;
         Action::OrderPlace(ts, price, quantity, side)
     }
 
     pub fn order_cancel(&self, price: u8, quantity: i32, side: Side) -> Action {
-        let ts= self.timer.get_time();
+        let ts= self.timer.get_time() + self.latency_constant;
         Action::OrderCancel(ts, price, quantity, side)
     }
 }
