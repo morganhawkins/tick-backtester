025f47371e3f3f3ca434003400159438aeb20098
morganhawkins <66576505+morganhawkins@users.noreply.github.com>
2025-08-29 10:47:43 -0400

    makign hist action producer a bufered reader and peekable

--- src/actions/historical_action_producer.rs
+++ src/actions/historical_action_producer.rs
@@ -1,19 +1,56 @@
-use std::cell::RefCell;
-use std::collections::VecDeque;
+use std::iter::Peekable;
+use std::error::Error;
 use std::rc::Rc;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Lines};
+use std::path::Path;
+
+use serde::de::DeserializeOwned;
 
 use super::actions::Action;
 use crate::time_keeping::timer::Timer;
 
+pub struct BufferedActionRecordReader {
+    read_buffer: Peekable<std::io::Lines<BufReader<File>>>
+}
+
+impl BufferedActionRecordReader {
+    pub fn new(path: String) -> Result<Self, Box<dyn Error>> {
+        let file_path = Path::new(&path);
+        let file = File::open(file_path)?;
+        let reader = BufReader::new(file);
+        let lines: Peekable<Lines<BufReader<File>>>  = reader.lines().into_iter().peekable();
+        Ok(Self{
+            read_buffer: lines
+        })
+    }
+}
+
+impl Iterator for BufferedActionRecordReader{
+    type Item = Action;
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
 // have this open a file and read it into a buffer
 pub struct HistoricalActionProducer {
     timer: Rc<Timer>,
-    action_buffer: RefCell<VecDeque<Action>>,
+    action_buffer: Peekable<Box<dyn Iterator<Item = Action>>>,
 }
 
-impl HistoricalActionProducer {
+
+
+impl HistoricalActionProducer{
+
+    pub fn new(timer: &Rc<Timer>, path: &str) -> ()
+    {
+        
+
+    }
+
     // pop all actions that will occur before the next time step
-    pub fn grab_cycle(&self) -> Vec<Action> {
+    pub fn grab_cycle(&mut self) -> Vec<Action> {
         let mut current_actions = Vec::new();
 
         // pop actions that will occur before next time step and return them
@@ -24,13 +61,14 @@ impl HistoricalActionProducer {
     }
 
     // only pops actions if it will occur before next time step
-    fn pop_action(&self) -> Option<Action> {
+    fn pop_action(&mut self) -> Option<Action> {
         let next_ts = self.timer.peek_next_time();
-        let next_action_ts = self.action_buffer.borrow().front()?.get_ts();
-        if next_action_ts <= next_ts {
-            Some(self.action_buffer.borrow_mut().pop_front()?)
-        } else {
-            None
-        }
+        
+        // match  self.action_buffer.peek() {
+
+        // }
+        None
     }
 }
+
+
